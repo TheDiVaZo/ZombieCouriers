9f1c8e58f7cc4ee0b0a4b80b7ded9b7c0eb5f5f5
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-09-20 16:48:29 +0300

    feat: Added mixins so that the zombies do not burn

---
+++ src/main/java/me/thedivazo/zombiecouriers/mixin/ZombieNoSunBurnMixin.java
@@ -0,0 +1,17 @@
+package me.thedivazo.zombiecouriers.mixin;
+
+import net.minecraft.entity.monster.ZombieEntity;
+import org.apache.logging.log4j.LogManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ZombieEntity.class)
+public abstract class ZombieNoSunBurnMixin {
+
+    @Inject(method = "isSunSensitive()Z", at = @At("HEAD"), cancellable = true, remap = false)
+    private void zombiecouriers$noSunBurnFromSun(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
+}

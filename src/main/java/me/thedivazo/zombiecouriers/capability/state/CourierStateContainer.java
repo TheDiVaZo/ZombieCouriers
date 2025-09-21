91b53395ba69443ea55601c7ced70447edd96498
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-09-21 22:36:18 +0300

    fix: Fixed error catch in Statecontainer

--- src/main/java/me/thedivazo/zombiecouriers/capability/state/CourierStateContainer.java
+++ src/main/java/me/thedivazo/zombiecouriers/capability/state/CourierStateContainer.java
@@ -1,11 +1,14 @@
 package me.thedivazo.zombiecouriers.capability.state;
 
 import net.minecraft.nbt.CompoundNBT;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CourierStateContainer implements ICourierStateContainer {
     private static final String STATE_NBT_KEY = "state";
+    private static final Logger LOGGER = LogManager.getLogger();
 
-    private State state = State.FIND_VILLAGE;
+    private State state = null;
 
     @Override
     public State getState() {
@@ -26,6 +29,11 @@ public class CourierStateContainer implements ICourierStateContainer {
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
-        this.state = State.valueOf(nbt.getString(STATE_NBT_KEY));
+        try {
+            state = State.valueOf(nbt.getString(STATE_NBT_KEY));
+        } catch (IllegalArgumentException e) {
+            state = null;
+            LOGGER.warn("Invalid nbt key: {}", nbt.getString(STATE_NBT_KEY), e);
+        }
     }
 }
